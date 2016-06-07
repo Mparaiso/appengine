ea59bba79715114f90b2ec591148374cedbfd230
mparaiso <aikah@free.fr>
2016-06-08 00:04:29 +0200

    relation resolutions moved to their own methods

--- repository.go
+++ repository.go
@@ -47,10 +47,10 @@ func (repository *Repository) Find(id Any, entity Entity) error {
 	return repository.FindOneBy(Query{Where: []string{repository.IDField, "=", "?"}, Params: []interface{}{id}}, entity)
 }
 
-// FindOneBy find one record filtered by query
+// FindOneBy finds one entity filtered by a query and resolve s
+// relations defined in that entity
 func (repository *Repository) FindOneBy(query QueryBuilder, entity Entity) error {
-	queryString, values, err := query.BuildQuery(repository)
-	err = repository.Connection.Get(entity, queryString, values...)
+	err := repository.doFindOneBy(query, entity)
 	if err != nil {
 		return err
 	}
@@ -59,45 +59,16 @@ func (repository *Repository) FindOneBy(query QueryBuilder, entity Entity) error
 	for _, relation := range metadata.Relations {
 		if relation.Fetch == Eager {
 			if relation.Type == OneToMany {
-				targetRepository, err := repository.ORM.GetRepositoryByEntityName(relation.TargetEntity)
-				if err != nil {
-					return err
-				}
-				// See http://stackoverflow.com/questions/25384640/why-golang-reflect-makeslice-returns-un-addressable-value
-				slice := reflect.MakeSlice(reflect.SliceOf(targetRepository.Type), 0, 0)
-				pointer := reflect.New(slice.Type())
-				pointer.Elem().Set(slice)
-
-				err = targetRepository.FindBy(
-					Query{Where: []string{relation.IndexBy, "=", "?"},
-						Params: []interface{}{repository.resolveId(entity)},
-					}, pointer.Interface(),
-				)
-
-				if err != nil {
-					return err
-				}
-				StructFieldValue := reflect.Indirect(reflect.ValueOf(entity)).FieldByName(relation.StructField)
-
-				StructFieldValue.Set(slice)
+				repository.resolveOneToManySingle(relation, entity)
 			}
 		}
 	}
 	return nil
 }
 
-func (repository *Repository) doFindOneBy(query QueryBuilder, entity Collection) error {
-	return nil
-}
-
-// FindBy find users by fields
+// FindBy find entities according to a query
 func (repository *Repository) FindBy(query QueryBuilder, collection Collection) error {
-	queryString, values, err := query.BuildQuery(repository)
-	if err != nil {
-		return err
-	}
-
-	err = repository.Connection.Select(collection, queryString, values...)
+	err := repository.doFindBy(query, collection)
 	if err != nil {
 		return err
 	}
@@ -106,55 +77,7 @@ func (repository *Repository) FindBy(query QueryBuilder, collection Collection) 
 	for _, relation := range metadata.Relations {
 		if relation.Fetch == Eager {
 			if relation.Type == OneToMany {
-				targetEntity := relation.TargetEntity
-				targetRepository, err := repository.ORM.GetRepositoryByEntityName(targetEntity)
-				if err != nil {
-					return err
-				}
-
-				// Get all collection IDs
-				ids := []interface{}{}
-				collectionValue := reflect.Indirect(reflect.ValueOf(collection))
-				collectionLength := collectionValue.Len()
-				for i := 0; i < collectionLength; i++ {
-					entityValue := reflect.Indirect(collectionValue.Index(i))
-					ids = append(ids, entityValue.FieldByName(repository.IDField).Interface())
-				}
-				// Build where query
-				whereQuery := []string{relation.IndexBy, "IN", "("}
-				for range ids {
-					whereQuery = append(whereQuery, "?", ",")
-				}
-				whereQuery[len(whereQuery)-1] = ")"
-
-				// See http://stackoverflow.com/questions/25384640/why-golang-reflect-makeslice-returns-un-addressable-value
-				slice := reflect.MakeSlice(reflect.SliceOf(targetRepository.Type), 0, 0)
-				pointer := reflect.New(slice.Type())
-				pointer.Elem().Set(slice)
-
-				// Get All related target records in one request
-				err = targetRepository.FindBy(Query{Where: whereQuery, Params: ids, OrderBy: map[string]Order{relation.IndexBy: ASC}}, pointer.Interface())
-				if err != nil {
-					return err
-				}
-				// assign target records to the returned collection entities
-				targetRecordMap := map[interface{}][]reflect.Value{}
-				targetSliceLength := slice.Len()
-				for i := 0; i < targetSliceLength; i++ {
-					targetRecord := slice.Index(i)
-					index := targetRecord.FieldByName(relation.IndexBy).Interface()
-					if _, ok := targetRecordMap[index]; !ok {
-						targetRecordMap[index] = []reflect.Value{}
-					}
-					targetRecordMap[index] = append(targetRecordMap[index], targetRecord)
-				}
-				// add target records to each entity of the returned collection
-				for i := 0; i < collectionLength; i++ {
-					entityValue := reflect.Indirect(collectionValue.Index(i))
-					sliceValue := reflect.MakeSlice(reflect.SliceOf(targetRepository.Type), 0, 0)
-					sliceValue = reflect.Append(sliceValue, targetRecordMap[entityValue.FieldByName(repository.IDField).Interface()]...)
-					entityValue.FieldByName(relation.StructField).Set(sliceValue)
-				}
+				repository.resolveOneToMany(relation, collection)
 			}
 		}
 	}
@@ -197,41 +120,28 @@ func (repository *Repository) Save(entities ...Entity) error {
 	return repository.ORM.Flush()
 }
 
-// UpdateAttribute update selected attributes
-func (repository *Repository) UpdateAttribute(entity Entity, fields map[string]interface{}) error {
-	values := []interface{}{}
-	setStatement := ""
-	userType := reflect.TypeOf(entity)
-	for key, value := range fields {
-		if _, ok := userType.Elem().FieldByName(key); !ok {
-			return fmt.Errorf("type %s doesn't have a field named %s ", userType.Name(), key)
-		}
-		values = append(values, value)
-		if setStatement == "" {
-			setStatement = fmt.Sprintf(" %s = ?", key)
-		} else {
-			setStatement = fmt.Sprintf(" %s, %s = ?", setStatement, key)
-		}
-	}
-	id := repository.resolveId(entity)
-	result, err := repository.Connection.Exec(fmt.Sprintf("UPDATE %s SET %s WHERE ID = ?;", repository.TableName, setStatement), append(values, id)...)
-	if err != nil {
-		return err
-	}
-	if _, err := result.RowsAffected(); err != nil {
-		return err
-	}
-	err = repository.Find(id, entity)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
 // Destroy deletes an entity
 func (repository *Repository) Destroy(entities ...Entity) error {
 	repository.ORM.Destroy(entities...)
 	return repository.ORM.Flush()
+}
+
+// doFindBy finds entities without fetching related entities
+func (repository *Repository) doFindBy(query QueryBuilder, collection Collection) error {
+	queryString, values, err := query.BuildQuery(repository)
+	if err != nil {
+		return err
+	}
+	return repository.Connection.Select(collection, queryString, values...)
+}
+
+// doFindOneBy doesn't resolve relations between entities
+func (repository *Repository) doFindOneBy(query QueryBuilder, entity Entity) error {
+	queryString, values, err := query.BuildQuery(repository)
+	if err != nil {
+		return err
+	}
+	return repository.Connection.Get(entity, queryString, values...)
 }
 
 // resolveId gets and returns the value of the Primary Key column
@@ -239,4 +149,92 @@ func (repository *Repository) Destroy(entities ...Entity) error {
 func (repository *Repository) resolveId(entity Entity) Any {
 	value := reflect.Indirect(reflect.ValueOf(entity))
 	return value.FieldByName(repository.IDField).Interface()
+}
+
+func (repository *Repository) resolveOneToMany(relation Relation, collection interface{}) error {
+	if relation.Fetch == Eager {
+		if relation.Type == OneToMany {
+			targetEntity := relation.TargetEntity
+			targetRepository, err := repository.ORM.GetRepositoryByEntityName(targetEntity)
+			if err != nil {
+				return err
+			}
+
+			// Get all collection IDs
+			ids := []interface{}{}
+			collectionValue := reflect.Indirect(reflect.ValueOf(collection))
+			collectionLength := collectionValue.Len()
+			for i := 0; i < collectionLength; i++ {
+				entityValue := reflect.Indirect(collectionValue.Index(i))
+				ids = append(ids, entityValue.FieldByName(repository.IDField).Interface())
+			}
+			// Build where query
+			whereQuery := []string{relation.IndexBy, "IN", "("}
+			for range ids {
+				whereQuery = append(whereQuery, "?", ",")
+			}
+			whereQuery[len(whereQuery)-1] = ")"
+
+			// See http://stackoverflow.com/questions/25384640/why-golang-reflect-makeslice-returns-un-addressable-value
+			slice := reflect.MakeSlice(reflect.SliceOf(targetRepository.Type), 0, 0)
+			pointer := reflect.New(slice.Type())
+			pointer.Elem().Set(slice)
+
+			// Get All related target records in one request
+			err = targetRepository.doFindBy(Query{Where: whereQuery, Params: ids, OrderBy: map[string]Order{relation.IndexBy: ASC}}, pointer.Interface())
+			if err != nil {
+				return err
+			}
+			// assign target records to the returned collection entities
+			targetRecordMap := map[interface{}][]reflect.Value{}
+			targetSliceLength := slice.Len()
+			for i := 0; i < targetSliceLength; i++ {
+				targetRecord := slice.Index(i)
+				index := targetRecord.FieldByName(relation.IndexBy).Interface()
+				if _, ok := targetRecordMap[index]; !ok {
+					targetRecordMap[index] = []reflect.Value{}
+				}
+				targetRecordMap[index] = append(targetRecordMap[index], targetRecord)
+			}
+			// add target records to each entity of the returned collection
+			for i := 0; i < collectionLength; i++ {
+				entityValue := reflect.Indirect(collectionValue.Index(i))
+				sliceValue := reflect.MakeSlice(reflect.SliceOf(targetRepository.Type), 0, 0)
+				sliceValue = reflect.Append(sliceValue, targetRecordMap[entityValue.FieldByName(repository.IDField).Interface()]...)
+				entityValue.FieldByName(relation.StructField).Set(sliceValue)
+			}
+			return nil
+		}
+	}
+	return fmt.Errorf("No relation to resolve for Relation '%v' for Type '%s' ", relation, repository.Type)
+}
+
+func (repository *Repository) resolveOneToManySingle(relation Relation, entity Entity) error {
+	if relation.Fetch == Eager {
+		if relation.Type == OneToMany {
+			targetRepository, err := repository.ORM.GetRepositoryByEntityName(relation.TargetEntity)
+			if err != nil {
+				return err
+			}
+			// See http://stackoverflow.com/questions/25384640/why-golang-reflect-makeslice-returns-un-addressable-value
+			slice := reflect.MakeSlice(reflect.SliceOf(targetRepository.Type), 0, 0)
+			pointer := reflect.New(slice.Type())
+			pointer.Elem().Set(slice)
+
+			err = targetRepository.doFindBy(
+				Query{Where: []string{relation.IndexBy, "=", "?"},
+					Params: []interface{}{repository.resolveId(entity)},
+				}, pointer.Interface(),
+			)
+
+			if err != nil {
+				return err
+			}
+			StructFieldValue := reflect.Indirect(reflect.ValueOf(entity)).FieldByName(relation.StructField)
+
+			StructFieldValue.Set(slice)
+			return nil
+		}
+	}
+	return fmt.Errorf("No relation to resolve for Relation '%v' for Type '%s' ", relation, repository.Type)
 }
