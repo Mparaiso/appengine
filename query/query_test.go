b6c9d8086ca31d75d28ebfbf216be1dfcdfed119
mparaiso <aikah@free.fr>
2016-06-25 19:09:54 +0200

    tests

--- query/query_test.go
+++ query/query_test.go
@@ -10,13 +10,15 @@ func ExampleQuery_BuildQuery(){
 	fmt.Println(err)
 	fmt.Println(result)
 	fmt.Println(values)
+	
 	// Output:
 	// <nil>
-	// SELECT * FROM articles ;
+	// SELECT * FROM articles  ;
+	// []
 }
 // A SELECT statement with columns and aliases and a WHERE statement with parameters
 func ExampleQuery_BuildQuery_second(){
-	result,values,err  := Query{Type:SELECT,Select:[]string{"a.title as Title","a.created_at as CreatedAt"},
+	result,values,err  := Query{Select:[]string{"a.title as Title","a.created_at as CreatedAt"},
 		From:[]string{"articles a"},
 		Where:[]string{"a.published","=","?"},
 		Params:[]interface{}{true},
@@ -30,4 +32,22 @@ func ExampleQuery_BuildQuery_second(){
 	// <nil>
 	// SELECT a.title as Title,a.created_at as CreatedAt FROM articles a WHERE a.published = ? ;
 	// [true]
+}
+
+func ExampleQuery_BuildQuery_third(){
+	result,values,err:=Query{Select:[]string{"a.title as Title"},
+		From:[]string{"articles a"},
+		Join:[]Join{{Table:"users u",On:"a.author_id = user.id"}},
+		Where:[]string{"u.id","=","?"},
+		Params:[]interface{}{1},
+	}.BuildQuery()
+
+	fmt.Println(err)
+	fmt.Println(result)
+	fmt.Println(values)
+
+	// Output:
+	// <nil>
+	// SELECT a.title as Title FROM articles a JOIN users u ON a.author_id = user.id WHERE u.id = ? ;
+	// [1]
 }
