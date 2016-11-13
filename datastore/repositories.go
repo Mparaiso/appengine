9e1a1eb66c6feb9ae205b047d4226da6cc5b3316
mparaiso <mparaiso@online.fr>
2016-11-14 00:07:26 +0100

    NewDefaultRepositoryWithSignal added

--- datastore/repositories.go
+++ datastore/repositories.go
@@ -3,9 +3,9 @@
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
 //    You may obtain a copy of the License at
-
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-
+//
 //    Unless required by applicable law or agreed to in writing, software
 //    distributed under the License is distributed on an "AS IS" BASIS,
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -36,6 +36,15 @@ type DefaultRepository struct {
 	Kind      string
 	Signal    Signal
 	ParentKey *datastore.Key
+}
+
+// NewDefaultRepositoryWithSignal allows to create a repository with an external signal
+func NewDefaultRepositoryWithSignal(ctx context.Context, kind string, signal Signal) *DefaultRepository {
+	defaultRepository := &DefaultRepository{Context: ctx, Kind: kind}
+	defaultRepository.Signal = signal
+	defaultRepository.Signal.Add(ListenerFunc(BeforeEntityCreatedListener))
+	defaultRepository.Signal.Add(ListenerFunc(BeforeEntityUpdatedListener))
+	return defaultRepository
 }
 
 // NewDefaultRepository creates a new DefaultRepository
